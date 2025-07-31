95c7977f58eaa33adb9538146c1b96b911002ed7
Michael Laptop kantor <michaelwillieson@gmail.com>
2025-07-31 10:05:12 +0700

    add canoncial home

--- src/app/page.js
+++ src/app/page.js
@@ -1,3 +1,4 @@
+import Head from "next/head";
 import Footer from "./components/Footer";
 import Hero from "./components/hero";
 import Navbar from "./components/navbar";
@@ -6,6 +7,9 @@ import Navbar from "./components/navbar";
 const Home = () => {
   return (
     <>
+    <Head>
+    <link rel="canonical" href="https://mitrajayaraya.com/" />
+    </Head>
     <Navbar/>
     <Hero/>
     <Footer/>
